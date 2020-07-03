79df02ee62cb13910dc5badcab85882126f9c8b7
ewinds <dutboxer@gmail.com>
2020-07-03 11:29:09 +0800

    Update config.prod.ts
    指定生产端口 31080

--- docker/config.prod.ts
+++ docker/config.prod.ts
@@ -1,5 +1,5 @@
 const config: IConfig = {
-  serve: `http://${window.location.hostname}:38080`,
+  serve: `http://${window.location.hostname}:31080`,
   keys: ['some secret hurr'],
   session: {
     key: 'koa:sess',
